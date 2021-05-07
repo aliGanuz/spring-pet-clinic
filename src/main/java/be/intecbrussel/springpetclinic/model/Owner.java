bafa09ce1847257b6278375cf81112d04f11301a
aliGanuz <aliganuz@gmail.com>
2021-05-07 13:21:35 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/be/intecbrussel/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package be.intecbrussel.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/be/intecbrussel/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package be.intecbrussel.springpetclinic.model;
+
+public class Vet extends Person {
+}
