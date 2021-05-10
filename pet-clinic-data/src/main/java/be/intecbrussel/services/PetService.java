bea3eebc2486f66c8976363a00c87dba27c36805
aliGanuz <aliganuz@gmail.com>
2021-05-10 13:09:31 +0200

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/be/intecbrussel/services/PetService.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/services/PetService.java
@@ -1,15 +1,10 @@
 package be.intecbrussel.services;
 
-import be.intecbrussel.model.Owner;
 import be.intecbrussel.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
     Pet findById(Long id);
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/be/intecbrussel/services/VetService.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/services/VetService.java
@@ -1,15 +1,8 @@
 package be.intecbrussel.services;
 
-import be.intecbrussel.model.Owner;
 import be.intecbrussel.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
